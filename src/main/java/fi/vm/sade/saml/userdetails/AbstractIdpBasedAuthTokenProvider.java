e5dab550fecd4fa567c6c5a4b82db7e417cb152b
jkaistinen <jkaistinen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-06-19 10:20:52 +0000

    OVT-586: Organisaation tallennus käyttäjälle.

--- src/main/java/fi/vm/sade/saml/userdetails/AbstractIdpBasedAuthTokenProvider.java
+++ src/main/java/fi/vm/sade/saml/userdetails/AbstractIdpBasedAuthTokenProvider.java
@@ -3,17 +3,23 @@
  */
 package fi.vm.sade.saml.userdetails;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.opensaml.saml2.core.Attribute;
 import org.opensaml.xml.XMLObject;
 import org.opensaml.xml.schema.XSString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.saml.SAMLCredential;
 
 import fi.vm.sade.authentication.service.AuthenticationService;
 import fi.vm.sade.authentication.service.UserManagementService;
 import fi.vm.sade.authentication.service.types.AddHenkiloData;
+import fi.vm.sade.authentication.service.types.AddHenkiloToOrganisaatiosData;
 import fi.vm.sade.authentication.service.types.dto.HenkiloDTO;
+import fi.vm.sade.organisaatio.api.model.OrganisaatioDTO;
+import fi.vm.sade.organisaatio.service.OrganisaatioService;
 
 /**
  * @author tommiha
@@ -21,9 +27,12 @@ import fi.vm.sade.authentication.service.types.dto.HenkiloDTO;
  */
 public abstract class AbstractIdpBasedAuthTokenProvider implements IdpBasedAuthTokenProvider {
 
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
     private List<String> supportedProviders;
     private UserManagementService userManagementService;
     private AuthenticationService authenticationService;
+    private OrganisaatioService organisaatioService;
 
     /*
      * (non-Javadoc)
@@ -49,12 +58,36 @@ public abstract class AbstractIdpBasedAuthTokenProvider implements IdpBasedAuthT
      */
     @Override
     public String createAuthenticationToken(SAMLCredential credential) {
-        HenkiloDTO henkilo = getUserManagementService().getHenkiloByIDPAndIdentifier(getIDPUniqueKey(), getUniqueIdentifier(credential));
+        HenkiloDTO henkilo = getUserManagementService().getHenkiloByIDPAndIdentifier(getIDPUniqueKey(),
+                getUniqueIdentifier(credential));
         if (henkilo == null) {
             AddHenkiloData addHenkiloData = createIdentity(credential);
             henkilo = getUserManagementService().addHenkilo(addHenkiloData);
+
+            List<AddHenkiloToOrganisaatiosData> ohdatas = new ArrayList<AddHenkiloToOrganisaatiosData>();
+
+            logger.info("domainNimi: " + addHenkiloData.getDomainNimi());
+            List<OrganisaatioDTO> list = organisaatioService.findOrganisaatiosByDomainNimi(addHenkiloData
+                    .getDomainNimi());
+
+            if (list != null && list.size() > 0) {
+                AddHenkiloToOrganisaatiosData ohdata = new AddHenkiloToOrganisaatiosData();
+
+                OrganisaatioDTO organisaatioDTO = list.get(0);
+                ohdata.setOrganisaatioOid(organisaatioDTO.getOid());
+
+                // ohdata.setMatkapuhelinnumero(oh.getMatkapuhelinnumero());
+                // ohdata.setTehtavanimike(oh.getTehtavanimike());
+                // ohdata.setPuhelinnumero(oh.getPuhelinnumero());
+                // ohdata.setSahkopostiosoite(oh.getSahkopostiosoite());
+
+                ohdatas.add(ohdata);
+
+                henkilo = userManagementService.addHenkiloToOrganisaatios(henkilo.getOidHenkilo(), ohdatas);
+            }
         }
-        return getAuthenticationService().generateAuthTokenForHenkilo(henkilo, getIDPUniqueKey(), getUniqueIdentifier(credential));
+        return getAuthenticationService().generateAuthTokenForHenkilo(henkilo, getIDPUniqueKey(),
+                getUniqueIdentifier(credential));
     }
 
     protected String getFirstAttributeValue(SAMLCredential credential, String attributeName) {
@@ -127,6 +160,14 @@ public abstract class AbstractIdpBasedAuthTokenProvider implements IdpBasedAuthT
      */
     public void setSupportedProviders(List<String> supportedProviders) {
         this.supportedProviders = supportedProviders;
+    }
+
+    public OrganisaatioService getOrganisaatioService() {
+        return organisaatioService;
+    }
+
+    public void setOrganisaatioService(OrganisaatioService organisaatioService) {
+        this.organisaatioService = organisaatioService;
     }
 
 }
--- src/main/java/fi/vm/sade/saml/userdetails/haka/HakaAuthTokenProvider.java
+++ src/main/java/fi/vm/sade/saml/userdetails/haka/HakaAuthTokenProvider.java
@@ -29,11 +29,16 @@ public class HakaAuthTokenProvider extends AbstractIdpBasedAuthTokenProvider {
     @Override
     protected AddHenkiloData createIdentity(SAMLCredential credential) {
         AddHenkiloData henkilo = new AddHenkiloData();
+
         henkilo.setEtunimet(getFirstAttributeValue(credential, "givenName"));
         henkilo.setSukunimi(getFirstAttributeValue(credential, "sn"));
         henkilo.setKutsumanimi(getFirstAttributeValue(credential, "givenName"));
-        henkilo.setHetu(getFirstAttributeValue(credential, "eduPersonPrincipalName"));
+        henkilo.setKayttajatunnus(getFirstAttributeValue(credential, "eduPersonPrincipalName"));
+
+        henkilo.setDomainNimi(getFirstAttributeValue(credential, "schacHomeOrganization"));
+
+        // TODO: remove this
+        henkilo.setDomainNimi("www.esa.fi");
         return henkilo;
     }
-
 }
