d1a1fd4a11a8565af55e14e5361f05f38e20bca6
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-08-11 12:26:37 +0300

    KJHH-1067: Add more config files (based on review)

--- src/main/java/fi/vm/sade/saml/configuration/ServiceProviderOphProperties.java
+++ src/main/java/fi/vm/sade/saml/configuration/ServiceProviderOphProperties.java
@@ -8,6 +8,7 @@ public class ServiceProviderOphProperties extends OphProperties {
     public ServiceProviderOphProperties() {
         addFiles("/service-provider-oph.properties");
         addOptionalFiles(Paths.get(System.getProperties().getProperty("user.home"), "/oph-configuration/common.properties").toString());
+        addOptionalFiles(Paths.get(System.getProperties().getProperty("user.home"), "/oph-configuration/service-provider-app.properties").toString());
     }
 
 }
