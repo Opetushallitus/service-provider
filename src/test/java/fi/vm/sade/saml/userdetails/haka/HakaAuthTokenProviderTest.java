9f71a2df756df688fd5ec1eb071e5cee025eee20
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-11-09 10:21:17 +0200

    KJHH-1471: Fix tests and use consistent naming

--- src/test/java/fi/vm/sade/saml/userdetails/haka/HakaAuthTokenProviderTest.java
+++ src/test/java/fi/vm/sade/saml/userdetails/haka/HakaAuthTokenProviderTest.java
@@ -2,6 +2,7 @@ package fi.vm.sade.saml.userdetails.haka;
 
 import fi.vm.sade.properties.OphProperties;
 import fi.vm.sade.saml.clients.KayttooikeusRestClient;
+import fi.vm.sade.saml.exception.EmailVerificationException;
 import fi.vm.sade.saml.exception.NoStrongIdentificationException;
 import fi.vm.sade.saml.userdetails.AbstractIdpBasedAuthTokenProvider;
 import fi.vm.sade.saml.userdetails.UserDetailsDto;
@@ -43,7 +44,7 @@ public class HakaAuthTokenProviderTest {
     }
 
     @Test(expected = NoStrongIdentificationException.class)
-    public void hakaRequireStrongIdentificationOidlistIsRead() throws Exception {
+    public void onStrongIdentificationListExistAndStrongIdentificationRedirectCodeShouldRedirectToStrongIdentification() throws Exception {
         this.hakaAuthTokenProvider.setHakaRequireStrongIdentificationListAsString("1.2.3.4.5,2.3.0.0.1");
         when(this.ophProperties.url(eq("kayttooikeus-service.cas.oidByIdp"), anyString(), anyString())).thenReturn("oidByIdpUrl");
         when(this.kayttooikeusRestClient.get(eq("oidByIdpUrl"), eq(String.class))).thenReturn("1.2.3.4.5");
@@ -53,16 +54,16 @@ public class HakaAuthTokenProviderTest {
     }
 
     @Test
-    public void hakeRedirectsAreDisabledAndUserIsNotInOidLists() throws Exception {
+    public void onStrongIdentificationDisabledAndEmailVerificationDisabledShouldNotRedirect() throws Exception {
         when(this.ophProperties.url(eq("kayttooikeus-service.cas.oidByIdp"), anyString(), anyString())).thenReturn("oidByIdpUrl");
         when(this.kayttooikeusRestClient.get(eq("oidByIdpUrl"), eq(String.class))).thenReturn("1.2.3.4.5");
         this.hakaAuthTokenProvider.createAuthenticationToken(null, this.defaultUserDetailsDto);
         verify(this.ophProperties, times(1)).url(anyString(), anyString(), anyString());
-        verify(this.kayttooikeusRestClient, times(2));
+        verify(this.kayttooikeusRestClient, times(2)).get(anyString(), eq(String.class));
     }
 
     @Test
-    public void hakaRequireStrongIdentificationOidlistDefaultIsRead() throws Exception {
+    public void onRequireStrongIdentificationAndStrongIdentificationListExistShouldCallForRedirectCode() throws Exception {
         this.hakaAuthTokenProvider.setRequireStrongIdentification(true);
         this.hakaAuthTokenProvider.setHakaRequireStrongIdentificationListAsString("1.2.3.4.5,2.3.4.45.56,2.3.4.6");
         when(this.kayttooikeusRestClient.get(anyString(), eq(String.class)))
@@ -72,30 +73,22 @@ public class HakaAuthTokenProviderTest {
         verify(this.ophProperties).url(eq("kayttooikeus-service.cas.login.redirect.oidHenkilo"), eq("1.2.3.4.5"));
     }
 
-    @Test
-    public void emailVerificationEnabledIsRead() throws Exception {
+    @Test(expected = EmailVerificationException.class)
+    public void onEmailVerificationEnabledAndRedirectCodeShouldRedirectToEmailVerification() throws Exception {
         this.hakaAuthTokenProvider.setEmailVerificationEnabled(true);
-        when(this.kayttooikeusRestClient.get(anyString(), eq(String.class)))
-                .thenReturn("1.2.3.4.5");
+        when(this.ophProperties.url(eq("kayttooikeus-service.cas.login.redirect.oidHenkilo"), anyString())).thenReturn("loginRedirectUrl");
+        when(this.kayttooikeusRestClient.get(eq("loginRedirectUrl"), eq(String.class))).thenReturn(AbstractIdpBasedAuthTokenProvider.EMAIL_VERIFICATION);
         this.hakaAuthTokenProvider.createAuthenticationToken(null, this.defaultUserDetailsDto);
         verify(this.ophProperties).url(eq("kayttooikeus-service.cas.oidByIdp"), anyString(), anyString());
         verify(this.ophProperties).url(eq("kayttooikeus-service.cas.login.redirect.oidHenkilo"), eq("1.2.3.4.5"));
     }
 
-    @Test
-    public void userIsInRequireStrongIdentificationList() throws Exception {
-        this.hakaAuthTokenProvider.setHakaRequireStrongIdentificationListAsString("1.2.3.4.5,2.3.4.45.56,2.3.4.6");
-        when(this.kayttooikeusRestClient.get(anyString(), eq(String.class)))
-                .thenReturn("2.3.4.6");
-        this.hakaAuthTokenProvider.createAuthenticationToken(null, this.defaultUserDetailsDto);
-        verify(this.ophProperties, times(1)).url(eq("kayttooikeus-service.cas.login.redirect.oidHenkilo"), anyString());
-}
-
-    @Test(expected = NoStrongIdentificationException.class)
-    public void hakaUserNotStronglyIdentifiedButIsRequiredTo() throws Exception {
-        this.hakaAuthTokenProvider.setRequireStrongIdentification(true);
+    @Test(expected = EmailVerificationException.class)
+    public void onUserInEmailVerificationListAndEmailVerificationRedirectCodeShouldRedirectToEmailVerification() throws Exception {
+        this.hakaAuthTokenProvider.setHakaEmailVerificationListAsString("2.3.4,1.2.3.4.5,3.7.5.23.67");
         when(this.kayttooikeusRestClient.get(anyString(), eq(String.class))).thenReturn("1.2.3.4.5");
-        when(this.kayttooikeusRestClient.get(anyString(), eq(String.class))).thenReturn(AbstractIdpBasedAuthTokenProvider.STRONG_IDENTIFICATION);
+        when(this.ophProperties.url("kayttooikeus-service.cas.login.redirect.oidHenkilo","1.2.3.4.5")).thenReturn("redirectCodeUrl");
+        when(this.kayttooikeusRestClient.get(eq("redirectCodeUrl"), eq(String.class))).thenReturn(AbstractIdpBasedAuthTokenProvider.EMAIL_VERIFICATION);
         this.hakaAuthTokenProvider.createAuthenticationToken(null, this.defaultUserDetailsDto);
     }
 }
